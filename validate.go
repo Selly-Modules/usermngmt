a4ae19667f09666cec3e7c6103b5b8630fd9c4f3
Hoang <lequanghoang99@outlook.com>
2021-11-09 09:08:56 +0700

    add updateMethod

--- validate.go
+++ validate.go
@@ -73,3 +73,53 @@ func (co CreateOptions) validate(ctx context.Context) error {
 
 	return nil
 }
+
+func (co UpdateOptions) validate(ctx context.Context) error {
+	// Name
+	if co.Name == "" {
+		logger.Error("usermngmt - Update: no name data", logger.LogData{
+			"payload": co,
+		})
+		return errors.New("no name data")
+	}
+
+	// Phone
+	if co.Phone == "" {
+		logger.Error("usermngmt - Update: no phone data", logger.LogData{
+			"payload": co,
+		})
+		return errors.New("no phone data")
+	}
+
+	// Email
+	if co.Email == "" {
+		logger.Error("usermngmt - Update: no email data", logger.LogData{
+			"payload": co,
+		})
+		return errors.New("no email data")
+	}
+
+	// RoleID
+	if co.RoleID == "" {
+		logger.Error("usermngmt - Update: no roleID data", logger.LogData{
+			"payload": co,
+		})
+		return errors.New("no role id data")
+	}
+
+	//  Find roleID exists or not
+	roleID, isValid := mongodb.NewIDFromString(co.RoleID)
+	if !isValid {
+		return errors.New("invalid role id data")
+	}
+	if !s.isRoleIDExisted(ctx, roleID) {
+		return errors.New("role id does not exist")
+	}
+
+	// Find phone number,email exists or not
+	if s.isPhoneNumberOrEmailExisted(ctx, co.Phone, co.Email) {
+		return errors.New("phone number or email already existed")
+	}
+
+	return nil
+}
